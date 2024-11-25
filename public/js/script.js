92c25e6a1b8061df053dae12b0c85199b6fd738d
danaguil <danaguilarfabian@gmail.com>
2024-11-25 13:23:14 -0800

    Created the the drum arrays and initialized it to false. Next: toggleDrum()

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+// Drum Array
+let kicks = []
+let snares = []
+let hiHats = []
+let rideCymbals = []
+// need to initialize the arrays before adding anything
+
+// loop to set all variables with 16 slots to false
+for(let i = 0; i < 16; i++){
+    kicks[i] = false;
+    snares[i] = false;
+    hiHats[i] = false;
+    rideCymbals[i] = false;
+}
+
+// toggle Drum function
+function toggleDrum() {
+    
+}
