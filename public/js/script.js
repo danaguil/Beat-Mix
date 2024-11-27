012e27929f619d7c6386377ccf4c7b9ee9735bfa
danaguil <danaguilarfabian@gmail.com>
2024-11-26 22:01:14 -0800

    Worked more on the toggleDrum function and also made a new function 'insertFalse'

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,27 @@ let hiHats = []
 let rideCymbals = []
 // need to initialize the arrays before adding anything
 
-// loop to set all variables with 16 slots to false
-for(let i = 0; i < 16; i++){
-    kicks[i] = false;
-    snares[i] = false;
-    hiHats[i] = false;
-    rideCymbals[i] = false;
+// loop to set all variables with 16 slots to false, using an insert function
+function insertFalse(array) {
+    for(let i = 0; i < 16; i++){
+        array[i] = false;
+    }
 }
+// using function to insert false
+insertFalse(kicks);
+insertFalse(snares);
+insertFalse(hiHats);
+insertFalse(rideCymbals);
 
-// toggle Drum function
-function toggleDrum() {
+// toggle Drum function, function will flip the boolean value in the correct
+// array at specified index
+function toggleDrum(array,indexNum) {
+    if(!    array) return null;
+
+    if(array === kicks || array === snares || array === hiHats || array === rideCymbals){
+        if (0 <= indexNum && indexNum < 16) {
+            array[indexNum] = !array[indexNum];
+        }
+    }
     
 }
